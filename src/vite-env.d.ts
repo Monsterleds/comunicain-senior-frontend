3ccd2db9eabee5b8ed8a12c28e16df46c5d35010
Khaled Mohamad <khaled@contemplato.com>
2024-05-22 17:11:43 -0300

    chore: removing useless enviroments

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_SWAPI_BASE_URL: string;
   readonly VITE_STORAGE_KEY_NAME: string;
-  readonly VITE_MODE: 'production' | 'development' | 'test' ;
   // more env variables...
 }
 
